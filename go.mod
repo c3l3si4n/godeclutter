82698852ea1aa20ce300694c77fa5b42d68059fc
Daniel Matsumoto <daniel.matsumoto@ifood.com.br>
2022-08-30 00:14:23 -0300

    added files

---
+++ go.mod
@@ -0,0 +1,11 @@
+module godeclutter
+
+go 1.17
+
+require github.com/rosshhun/gonormalizer v0.0.0-20220512155713-cb6e05089833
+
+require (
+	github.com/PuerkitoBio/purell v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ godeclutter.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/purell"
+)
+
+// var strFlag = flag.String("long-string", "", "Description")
+var preferHttpsFlag = flag.Bool("p", false, "Prefer HTTPS - If there's a https url present, don't print the http for it. (since it will probably just redirect to https)")
+var normalizeURLFlag = flag.Bool("c", false, "Clean URLs - Aggressively clean/normalize URLs before outputting them.")
+var blacklistExtensionsFlag = flag.Bool("b", true, "Blacklist Extensions - clean some uninteresting extensions.")
+
+var blacklistedExtensions = []string{"css", "png", "jpg", "jpeg", "svg", "ico", "webp", "ttf", "otf", "woff", "gif", "pdf", "bmp", "eot", "mp3", "woff2", "mp4", "avi"}
+
+func iterInput(c chan string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		c <- scanner.Text()
+	}
+	close(c)
+}
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func stringInSlice(a string, list []string) (int, bool) {
+	for i, b := range list {
+		if b == a {
+			return i, true
+		}
+	}
+	return 0, false
+}
+
+func remove(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func normalizeURL(url string) string {
+	normalized := purell.MustNormalizeURLString(url, purell.FlagLowercaseScheme|purell.FlagLowercaseHost|purell.FlagUppercaseEscapes|purell.FlagDecodeUnnecessaryEscapes|purell.FlagEncodeNecessaryEscapes|purell.FlagRemoveDefaultPort|purell.FlagRemoveEmptyQuerySeparator|purell.FlagRemoveDotSegments|purell.FlagRemoveDuplicateSlashes|purell.FlagSortQuery)
+	return normalized
+
+}
+
+func main() {
+
+	flag.Parse()
+
+	var c chan string = make(chan string)
+	go iterInput(c)
+
+	var processedUrls []string
+
+	for line := range c {
+
+		u, err := url.ParseRequestURI(line)
+		if err != nil {
+			continue
+		}
+
+		// Remove http-on-https false-positives
+		if u.Scheme == "http" && u.Port() == "443" {
+			continue
+		}
+		if u.Scheme == "https" && u.Port() == "80" {
+			continue
+		}
+
+		// Escape redundant port syntax (for default ports)
+		if strings.Contains(u.Host, ":") {
+			host, port, err := net.SplitHostPort(u.Host)
+			if err != nil {
+				continue
+			}
+
+			if port == "443" && u.Scheme == "https" {
+				u.Host = host
+			} else if port == "80" && u.Scheme == "http" {
+				u.Host = host
+			} else if port == "" {
+				fmt.Print(host)
+				u.Host = host
+			} else {
+				u.Host = host + ":" + port
+			}
+
+		}
+
+		// Prefer https
+		if *preferHttpsFlag {
+			if u.Scheme == "https" {
+				check_u, _ := url.Parse(u.String())
+				check_u.Scheme = "http"
+
+				found_index, found := stringInSlice(check_u.String(), processedUrls)
+				if found {
+					processedUrls = remove(processedUrls, found_index)
+				}
+
+			} else if u.Scheme == "http" {
+				check_u, _ := url.Parse(u.String())
+				check_u.Scheme = "https"
+
+				_, found := stringInSlice(check_u.String(), processedUrls)
+				if found {
+					continue
+				}
+			}
+		}
+
+		if *normalizeURLFlag {
+			u, _ = url.Parse(normalizeURL(u.String()))
+		}
+
+		if *blacklistExtensionsFlag {
+			foundBlacklisted := false
+			for _, ext := range blacklistedExtensions {
+				if strings.HasSuffix(u.Path, ext) {
+					foundBlacklisted = true
+					continue
+				}
+			}
+			if foundBlacklisted {
+				continue
+			}
+		}
+
+		processedUrls = append(processedUrls, u.String())
+	}
+
+	filteredProcessedUrls := removeDuplicateStr(processedUrls)
+
+	for _, url := range filteredProcessedUrls {
+		fmt.Println(url)
+	}
+
+}
